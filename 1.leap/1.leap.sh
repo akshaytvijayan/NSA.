cde2c5a0cc28ddb1c5c3f72283d83d6e81bb44bd
AKSHAY T VIJAYAN <113921142+akshaytvijayan@users.noreply.github.com>
2023-06-12 15:29:23 +0530

    1.leap.sh

--- 1.leap/1.leap.sh
+++ 1.leap/1.leap.sh
@@ -14,7 +14,7 @@ fi
 year4=`expr $year % 4`
 year100=`expr $year % 100`
 year400=`expr $year % 400`
-if [ $year4 -eq 0 -a $year -ne 0 -o $year400 -eq 0 ]
+if [ $year4 -eq 0 -a $year100 -ne 0 -o $year400 -eq 0 ]
 then
    echo " $year is a leap year"
 else
