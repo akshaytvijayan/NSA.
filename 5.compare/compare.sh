f01f1bb999358287504fddf48a4ad9ef9f35761f
AKSHAY T VIJAYAN <113921142+akshaytvijayan@users.noreply.github.com>
2023-06-19 10:37:50 +0530

    Add files via upload

---
+++ 5.compare/compare.sh
@@ -0,0 +1,18 @@
+if [ $# -ne 0 ]
+then
+echo "Syntax error"
+exit
+fi
+	echo "Enter a file name : "
+	read a
+	echo "Enter another file name : "
+	read b
+	cmp $a $b
+	if [ $? -eq 0 ]
+	then
+	rm $b
+	echo "Removed duplicate file"
+	else
+	echo "Duplicate file not exists"
+	fi
+
