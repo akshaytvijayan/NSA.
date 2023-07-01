2c9059859af737c4b4090f4b49b6eb2f11f8d5c5
AKSHAY T VIJAYAN <113921142+akshaytvijayan@users.noreply.github.com>
2023-07-01 11:37:54 +0530

    Update sir.sh

--- 13.occurrence/sir.sh
+++ 13.occurrence/sir.sh
@@ -1,3 +1,8 @@
+if [ $# -eq 0 ] 
+then
+echo "syntax is <$0> <word> <file1> <file2> etc"
+exit 1
+fi
 word=$1
 shift
 count=0
@@ -13,3 +18,21 @@ done
 shift
 done
 echo "Number of occurance of word = $count"
+exit 0
+
+
+OUTPUT
+
+ksb@ksb-H410M-S2-V2:~/scripts$ cat>x
+kevin ur super
+kevin ur intelligent
+kevin ur good looking
+^Z
+[5]+  Stopped                 cat > x
+ksb@ksb-H410M-S2-V2:~/scripts$ cat>y
+are we all know malayalam
+are ok kevin
+^Z
+[6]+  Stopped                 cat > y
+ksb@ksb-H410M-S2-V2:~/scripts$ sh occur.sh  kevin x  y
+number of occurence of words = 4
