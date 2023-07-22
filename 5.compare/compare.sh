4f16bac53fd63320a27a365a3727e030dba2ab27
AKSHAY T VIJAYAN <113921142+akshaytvijayan@users.noreply.github.com>
2023-07-22 11:23:15 +0530

    Update compare.sh

--- 5.compare/compare.sh
+++ 5.compare/compare.sh
@@ -1,18 +1,23 @@
-if [ $# -ne 0 ]
+if [ $# -gt 2 -o $# -eq 1 ]
 then
-echo "Syntax error"
+echo ""Syntax Error.\nSyntax --> $0 [filename] [filename]
 exit
 fi
-	echo "Enter a file name : "
-	read a
-	echo "Enter another file name : "
-	read b
-	cmp $a $b
-	if [ $? -eq 0 ]
-	then
-	rm $b
-	echo "Removed duplicate file"
-	else
-	echo "Duplicate file not exists"
-	fi
-
+if [ $# -eq 0 ]
+then
+echo "Enter a file name :: "
+read a
+echo "Enter another file name :: "
+read b
+else
+a=$1
+b=$2
+fi
+cmp $a $b > /dev/null
+if [ $? -eq 0 ]
+then
+rm $b
+echo "Removed duplicate file"
+else
+echo "Not a duplicate file"
+fi
