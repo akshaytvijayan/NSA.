cc0d40edc2200411747a29dceb5786fa839dcc26
AKSHAY T VIJAYAN <113921142+akshaytvijayan@users.noreply.github.com>
2023-07-22 12:08:11 +0530

    Update compare.sh

--- 5.compare/compare.sh
+++ 5.compare/compare.sh
@@ -1,19 +1,16 @@
-if [ $# -gt 2 -o $# -eq 1 ]
+if [ $# -ne 0 ]
 then
-echo ""Syntax Error.\nSyntax --> $0 [filename] [filename]
+echo "Syntax Error."
 exit
 fi
-if [ $# -eq 0 ]
-then
+
 echo "Enter a file name :: "
 read a
 echo "Enter another file name :: "
 read b
-else
-a=$1
-b=$2
-fi
-cmp $a $b > /dev/null
+
+cmp $a $b >/dev/null
+
 if [ $? -eq 0 ]
 then
 rm $b
